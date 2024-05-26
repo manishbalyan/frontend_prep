f57da9d21ae66353e970d92ed56088123f063e63
Manish Kumar <manish.kumar@12-M-FVFGW2FRQ05N.local>
2024-05-27 00:20:06 +0530

    curry with placeholder done

--- JS/currying.js
+++ JS/currying.js
@@ -8,11 +8,29 @@ function curry(fn) {
         };
       }
     };
-  }
+}
+
+const _ = Symbol('placeholder');
+
+function curryWithPlaceholder(fn) {
+    return function curried(...args){
+        const complete = args.length >= fn.length && !args.includes(_);
+        if(complete){
+            return fn.apply(this,args);
+        }
+        else{
+            return function(...nextArgs){
+                const newArgs = args.map((arg)=>arg === _ && nextArgs.length ? nextArgs.shift() : arg).concat(nextArgs);
+                return curried.apply(this, newArgs);
+            }
+        }
+    }
+
+}
 
 function add(a, b, c) {
     return a + b + c;
-  }
+}
 
 const curriedAdd = curry(add);
 
@@ -20,3 +38,14 @@ console.log(curriedAdd(1)(2)(3));
 console.log(curriedAdd(1, 2)(3)); 
 console.log(curriedAdd(1)(2, 3)); 
 console.log(curriedAdd(1, 2, 3)); 
+
+function multiply(a, b, c, d) {
+    return a * b * c * d;
+}
+
+const curriedMultiply = curryWithPlaceholder(multiply);
+
+console.log(curriedMultiply(2)(3)(4)(5));           
+console.log(curriedMultiply(2, _, 4)(3)(5));        
+console.log(curriedMultiply(_, 3, _, 5)(2)(4));
+console.log(curriedMultiply(_, 3, 4)(_)(2)(5)); 
