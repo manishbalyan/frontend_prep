d990c1d6dae14d2bcac16bf22401a79ec9facb6d
manishbalyan <balyan05.manish@gmail.com>
2024-06-02 17:33:53 +0530

    fix: promise.allsetteled pollyfill done

--- JS/promisePollyfill.js
+++ JS/promisePollyfill.js
@@ -35,4 +35,39 @@ promiseAll([promise1,promise2,promise3]).then((values)=>{
     console.log(values);
 }).catch((error)=>{
     console.error(error)
-})
+});
+
+
+function promiseAllSetteled(promises){
+    return new Promise((resolve,reject)=>{
+        if(!Array.isArray(promises)){
+            reject(new TypeError('arguments must be an array'));
+        }
+        let result = [];
+        let setteledPromise = 0;
+        let promiseLength = promises.length;
+        if(promiseLength===0){
+            resolve(result);
+        }
+        promises.forEach((promise,index) => {
+            Promise.resolve(promise).then((value)=>{
+                result[index] = {status: 'fulfilled', value}
+            }).catch((error)=>{
+                result[index] = {status: 'rejected', error}
+            }).finally(()=>{
+                setteledPromise++;
+                if(setteledPromise===promise.length){
+                    resolve(result);
+                }
+            })
+        });
+    })
+}
+
+const promise4 = Promise.resolve(3);
+const promise5 = new Promise((resolve, reject) => setTimeout(reject, 100, 'foo'));
+const promise6 = 42;
+
+Promise.allSettled([promise4, promise5, promise6]).then(results => {
+  console.log(results);
+});
